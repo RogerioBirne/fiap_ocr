4037c21ed90e47610db3fdc77556d89cb06ebb4d
Rogerio Birne <rogerio.birne@mercadolivre.com>
2021-10-10 23:07:24 -0300

    Improve accuracy 34%

--- src/ocr/image_qr_code_eraser.py
+++ src/ocr/image_qr_code_eraser.py
@@ -13,13 +13,15 @@ def erase_qrcode(img, margin=0):
 
     contours = image_contour_filter.find_contours(invert)
     min_qr_code_side = int(img.shape[1] * 0.20)
+    min_qr_code_area = min_qr_code_side ** 2
     for c in contours:
         peri = cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(c, 0.06 * peri, True)
         x, y, w, h = cv2.boundingRect(approx)
         area = cv2.contourArea(c)
         ar = w / float(h)
-        if len(approx) == 4 and area > 10000 and (0.85 < ar < 1.6) and w >= min_qr_code_side and h >= min_qr_code_side:
+        if len(approx) == 4 and area > min_qr_code_area and (0.85 < ar < 1.6) \
+                and w >= min_qr_code_side and h >= min_qr_code_side:
             img = image.draw_box(img, (x - margin, y - margin), (x + w + margin, y + h + margin),
                                  color=__COLOR_WHITE_RGB__, border_size=cv2.FILLED)
     return img
