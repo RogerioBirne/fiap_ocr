eae7e2d6afc7a6930740b2591b7386538001441c
Rogerio Birne <rogerio.birne@mercadolivre.com>
2021-10-11 01:03:21 -0300

    add debugs

--- src/test_ocr.py
+++ src/test_ocr.py
@@ -4,14 +4,19 @@ import textdistance
 
 
 def debug_ocr_with_accuracy(image_filename, text_filename):
+    print('start scan')
     fiscal_ticket = fiscal_ticket_ocr.convert_file_image_to_string(
-        '{}/images/{}'.format(RESOURCES_PATH, image_filename))
+        '{}/images/{}'.format(RESOURCES_PATH, image_filename), debug=True)
 
     with open('{}/images/{}'.format(RESOURCES_PATH, text_filename), 'r') as file:
         digit_text = file.read()
+
+        print(fiscal_ticket)
+        print('scanned image: [{}] and compare to: [{}]'.format(image_filename, text_filename))
 
         distance = textdistance.hamming.distance(digit_text, fiscal_ticket)
         print('distance', distance)
 
         normalized_similarity = textdistance.hamming.normalized_similarity(digit_text, fiscal_ticket)
         print('normalized_similarity', normalized_similarity)
+    print('--------------------------------------------------')
