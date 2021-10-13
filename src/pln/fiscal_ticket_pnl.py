88285a78525d1b5fa701f82332ef969b86c4556f
Rogerio Birne <rogerio.birne@mercadolivre.com>
2021-10-13 14:36:30 -0300

    create some constants

--- src/pln/fiscal_ticket_pnl.py
+++ src/pln/fiscal_ticket_pnl.py
@@ -1,13 +1,18 @@
 import re
 from pycpfcnpj import cpfcnpj
 
-array_regex_cpf = ["[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}"]
-array_regex_cnpj = ["[0-9]{2}\.?[0-9]{3}\.?[0-9]{3}\/?[0-9]{4}\-?[0-9]{2}"]
-array_regex_total = ["([Tt][Oo][Tt][Aa][Ll]).*([R$]).*([-+]?\d*\.?\d+|[-+]?\d+)",
-                     "([Vv][Aa][Ll][Oo][Rr]).*([R$]).*([-+]?\d*\.?\d+|[-+]?\d+)",
-                     "([Pp][Aa][Gg][Aa][Rr]).*([R$]).*([-+]?\d*\.?\d+|[-+]?\d+)"]
-array_regex_itens = ["([-+]?\d*\.?\d+|[-+]?\d+).*([Uu][Nn]).*([-+]?\d*\.?\d+|[-+]?\d+)"]
-numbers_points = "0123456789.,"
+__ARRAY_REGEX_CPF__ = ['[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}']
+__ARRAY_REGEX_CNPJ__ = ['[0-9]{2}\.?[0-9]{3}\.?[0-9]{3}\/?[0-9]{4}\-?[0-9]{2}']
+__ARRAY_REGEX_TOTAL__ = ['([Tt][Oo][Tt][Aa][Ll]).*([R$]).*([-+]?\d*\.?\d+|[-+]?\d+)',
+                         '([Vv][Aa][Ll][Oo][Rr]).*([R$]).*([-+]?\d*\.?\d+|[-+]?\d+)',
+                         '([Pp][Aa][Gg][Aa][Rr]).*([R$]).*([-+]?\d*\.?\d+|[-+]?\d+)']
+__ARRAY_REGEX_ITEMS__ = ['([-+]?\d*\.?\d+|[-+]?\d+).*([Uu][Nn]).*([-+]?\d*\.?\d+|[-+]?\d+)']
+__NUMBERS_POINTS__ = '0123456789.,'
+__NEW_LINE__ = '\n'
+__UNIT__ = 'UN'
+__CPF_SIZE__ = 11
+__CNPJ_MIN_SIZE__ = 14
+__CNPJ_MAX_SIZE__ = 18
 
 
 def extract_data(text):
@@ -16,83 +21,82 @@ def extract_data(text):
 
 
 def structure_text(text):
-    new_text = ""
-    for l in text.split("\n"):
-        if len(l) > 5:
-            if "UN" in l[:5]:
-                new_text = new_text[:len(new_text) - 2] + l + "\n"
+    new_text = ''
+    for line in text.split(__NEW_LINE__):
+        if len(line) > 5:
+            if __UNIT__ in line[:5]:
+                new_text = new_text[:len(new_text) - 2] + line + __NEW_LINE__
             else:
-                new_text = new_text + l + "\n"
+                new_text = new_text + line + __NEW_LINE__
     return new_text
 
 
 def apply_extraction(text):
     result = {}
-    array_text = text.split("\n")
-    result["customer"] = extract_cpf(array_text)
-    result["company"] = extract_cnpj(array_text)
-    result["itens"] = extract_itens(array_text)
-    result["total"] = extract_total(array_text)
+    array_text = text.split(__NEW_LINE__)
+    result['customer'] = extract_cpf(array_text)
+    result['company'] = extract_cnpj(array_text)
+    result['items'] = extract_items(array_text)
+    result['total'] = extract_total(array_text)
     return result
 
 
 def extract_cpf(array_text):
     for line in array_text:
-        for r in array_regex_cpf:
-            m = re.search(r, line)
-            if m:
-                value = m.group(0)
+        for regex in __ARRAY_REGEX_CPF__:
+            matches = re.search(regex, line)
+            if matches:
+                value = matches.group(0)
                 if cpfcnpj.validate(value):
-                    if len(value) >= 11 and len(value) <= 14:
-                        if len(value) == 11:
+                    if __CPF_SIZE__ <= len(value) <= __CNPJ_MIN_SIZE__:
+                        if len(value) == __CPF_SIZE__:
                             return format_cpf(value)
                         return value
-    return ""
+    return ''
 
 
 def extract_cnpj(array_text):
     for line in array_text:
-        for r in array_regex_cnpj:
-            m = re.search(r, line)
-            if m:
-                value = m.group(0)
+        for regex in __ARRAY_REGEX_CNPJ__:
+            matches = re.search(regex, line)
+            if matches:
+                value = matches.group(0)
                 if cpfcnpj.validate(value):
-                    if len(value) >= 14 and len(value) <= 18:
-                        if len(value) == 14:
+                    if __CNPJ_MIN_SIZE__ <= len(value) <= __CNPJ_MAX_SIZE__:
+                        if len(value) == __CNPJ_MIN_SIZE__:
                             return format_cnpj(value)
-                        else:
-                            return value
-    return ""
+                        return value
+    return ''
 
 
-def extract_itens(array_text):
-    itens = []
+def extract_items(array_text):
+    items = []
     for line in array_text:
-        for r in array_regex_itens:
-            m = re.search(r, line)
-            if m:
-                value = m.group(0)
-                if value != "":
-                    itens.append(value)
-    return itens
+        for regex in __ARRAY_REGEX_ITEMS__:
+            matches = re.search(regex, line)
+            if matches:
+                value = matches.group(0)
+                if value != '':
+                    items.append(value)
+    return items
 
 
 def extract_total(array_text):
     for line in array_text:
-        for index, r in enumerate(array_regex_total):
-            m = re.search(r, line)
-            if m:
-                value = m.group(0)
-                if value != "":
+        for index, regex in enumerate(__ARRAY_REGEX_TOTAL__):
+            matches = re.search(regex, line)
+            if matches:
+                value = matches.group(0)
+                if value != '':
                     return only_numbers_and_points(value)
-    return ""
+    return ''
 
 
 def only_numbers_and_points(text):
-    value = ""
-    for c in text:
-        if c in numbers_points:
-            value += c
+    value = ''
+    for character in text:
+        if character in __NUMBERS_POINTS__:
+            value += character
     return value
 
 
