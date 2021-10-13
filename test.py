ef32a901a6813554523ae353c0c5462207c23d86
Rogerio Birne <rogerio.birne@mercadolivre.com>
2021-10-13 15:57:18 -0300

    add test script

---
+++ test.py
@@ -0,0 +1,11 @@
+from src import test_ocr
+
+if __name__ == '__main__':
+    debug = True
+
+    fiscal_ticket = test_ocr.debug_ocr_with_accuracy('example_02.jpg', 'example_02.txt', debug=debug)
+    fiscal_ticket = test_ocr.debug_ocr_with_accuracy('example_03.jpg', 'example_03.txt', debug=debug)
+    fiscal_ticket = test_ocr.debug_ocr_with_accuracy('example_04.jpg', 'example_04.txt', debug=debug)
+    fiscal_ticket = test_ocr.debug_ocr_with_accuracy('example_05.jpg', 'example_05.txt', debug=debug)
+    fiscal_ticket = test_ocr.debug_ocr_with_accuracy('example_06.jpg', 'example_06.txt', debug=debug)
+    fiscal_ticket = test_ocr.debug_ocr_with_accuracy('example_07.jpg', 'example_07.txt', debug=debug)
